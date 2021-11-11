c22d104d0986ff5e23faa994d385a5347da6036e
Sirwankhaleefa <33750982+Sirwankhaleefa@users.noreply.github.com>
2021-11-11 11:40:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,9 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+
+    
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+st.line_chart(chart_data)
